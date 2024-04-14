e4d4ac97db55450a83226cc03174634a7645c0dd
amey_pandit <panditamey1@gmail.com>
2024-04-14 09:33:52 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,17 +1,8 @@
 import streamlit as st
 import pandas as pd
 import os
-import glob
+from manage_csv import get_csv_files, csv_directory
 
-csv_directory = 'csv_files'
-
-# Ensure the directory for CSV files exists
-if not os.path.exists(csv_directory):
-    os.makedirs(csv_directory)
-
-def get_csv_files():
-    """Returns a list of csv files from the csv_directory."""
-    return glob.glob(os.path.join(csv_directory, '*.csv'))
 def group_after_nongroup_analysis(file_path, numbers_list):
     """Analyzes how many times a number from the predefined list follows a number not in the list."""
     df = pd.read_csv(file_path)
@@ -26,6 +17,18 @@ def group_after_nongroup_analysis(file_path, numbers_list):
 def app():
     st.title('Analysis of Number Sequences')
 
+    # File upload functionality
+    uploaded_file = st.file_uploader("Upload a CSV file for analysis", type=['csv'])
+    if uploaded_file is not None:
+        file_path = os.path.join(csv_directory, uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        st.success('File uploaded successfully. You can now select it below for analysis.')
+
+    # Select CSV file for analysis
+    existing_files = get_csv_files()
+    selected_file = st.selectbox('Or select an existing CSV file:', existing_files)
+
     # Predefined lists
     predefined_lists = {
         "List 1": [22, 18, 29, 7, 28, 12, 35, 3, 26, 0, 32, 15, 19, 4, 21, 2, 25],
@@ -33,10 +36,6 @@ def app():
         "List 3": [27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33],
         "Custom": []
     }
-
-    # Select CSV file for analysis
-    existing_files = get_csv_files()
-    selected_file = st.selectbox('Select a CSV file for analysis:', existing_files)
 
     # Dropdown for selecting predefined list
     selected_list_name = st.selectbox('Choose a predefined list or select custom to define your own:', list(predefined_lists.keys()))
